511df6f81223834035220f035b0ee62a7f34538b
LillianMcPadden <89875986+LillianMcPadden@users.noreply.github.com>
2022-01-26 20:29:16 -0500

    Week 2 In Class Program

---
+++ AreaPerimeter.java
@@ -0,0 +1,8 @@
+public class AreaPerimeter{
+    public static void main (String[] args){
+        double radius = 7.5;
+        System.out.println("Radius = " + radius);
+        System.out.println("Area = " + (radius * radius * Math.PI));
+        System.out.println("Perimeter = " + (radius * 2 * Math.PI));
+    }
+}

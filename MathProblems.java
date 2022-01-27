56a9bd28ca6ee28abd47a98596dab8a64afdef36
LillianMcPadden <89875986+LillianMcPadden@users.noreply.github.com>
2022-01-26 20:16:53 -0500

    Week 2 In Class Programs

---
+++ MathProblems.java
@@ -0,0 +1,11 @@
+public class MathProblems{
+    public static void main(String[] args){
+        int num1 = 125;
+        int num2 = 24;
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        System.out.println(num1 + " mod " + num2 + " = " + (num1 % num2));
+    }
+}

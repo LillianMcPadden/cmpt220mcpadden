c0a0f3779e47db7b66f0c454e42024629b631e37
LillianMcPadden <89875986+LillianMcPadden@users.noreply.github.com>
2022-02-02 17:24:35 -0500

    Assignment 1 Homework due 2/4/22

---
+++ hw/1/Acceleration.java
@@ -0,0 +1,10 @@
+public class Acceleration{
+    public static void main(String[] args){
+        double v0 = 4.6;
+        double v1 = 11.5;
+        double t = 0.6;
+        double a = (v1 - v0) / t;
+
+        System.out.println("The accelaration is " + a + " meters/second");
+    }
+}
---
+++ hw/1/Computation.java
@@ -0,0 +1,10 @@
+public class Computation{
+    public static void main(String[] args){
+        int a = 2;
+        int b = 5;
+        int c = 4;
+        int d = (b * b) - (4 * a * c);
+
+        System.out.println("The discriminant of " + a + "x^2 + " + b + "x + " + c + "= 0 is " + d);
+    }
+}

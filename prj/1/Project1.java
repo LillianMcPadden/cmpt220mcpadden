41fb58a6a61e0a680c03c7b8c573fa7620a3cd9e
LillianMcPadden <89875986+LillianMcPadden@users.noreply.github.com>
2022-02-16 18:47:09 -0500

    Project 1 Due 2/25/22

---
+++ prj/1/Project1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Project1{
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number and the mean of the two numbers: ");
+        int S1 = input.nextInt();
+        int T = input.nextInt();
+
+        int S2 = T * 2 - S1;
+        System.out.println("The missing variable is " + S2);
+        input.close();
+    }
+}

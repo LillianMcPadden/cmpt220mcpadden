dd883caeec6d262b52736f5e690829e5cb21f74a
Juan Arias <juan.arias@marist.edu>
2022-03-13 21:26:04 -0400

    Feedback

--- prj/1/Project1.java
+++ prj/1/Project1.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
+// JA: Comments?
 public class Project1{
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the first number and the mean of the two numbers: ");
-        int S1 = input.nextInt();
+        int S1 = input.nextInt(); // JA: Your variables should be lowercase
         int T = input.nextInt();
 
         int S2 = T * 2 - S1;

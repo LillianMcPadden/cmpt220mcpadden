18d6e442ef2c2458f22e37a54b8821430b5fb74a
Juan Arias <juan.arias@marist.edu>
2022-04-16 17:05:35 -0400

    Feedback

--- prj/3/Project3.java
+++ prj/3/Project3.java
@@ -1,87 +1,83 @@
 import java.util.Scanner;
 
-public class Project3{
-    public static void main(String [] args){
+public class Project3 {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        //loops 5 times for 5 houses
-        for(int i = 1; i <= 5; i++){
+        // loops 5 times for 5 houses
+        for (int i = 1; i <= 5; i++) {
             // gets dimensions of each house and creates 2d array
             System.out.println("Enter the dimensions of house: ");
             int W = input.nextInt();
             int L = input.nextInt();
-            char [][] House = new char [W][L];
+            char[][] House = new char[W][L];
             System.out.println("House " + i);
             String x = input.nextLine();
             // asks user to enter the layout of the house
             System.out.println("Enter house layout: ");
-            for (int col = 0; col < House.length; col++){
+            for (int col = 0; col < House.length; col++) {
                 x = input.nextLine();
-                for(int row = 0; row < House[col].length; row++){
+                for (int row = 0; row < House[col].length; row++) {
                     House[col][row] = x.charAt(row);
-                    char [][] newHouse = Maze(House[col][row]);
+                    char[][] newHouse = Maze(House[col][row]);
                     System.out.println(newHouse);
                 }
             }
-            
-            input.close();        
+
+            input.close();
         }
-        public static char[][] Maze(char [][] House){
-            char enter = '*';
-            char mirror1 = '/';
-            char mirror2 = '\\';
-            char open = '.';
-            char exit = '&';
-            for (int col = 0; col < House.length; col++){
-                for(int row = 0; row < House[col].length; row++){
-                    char [][] spot = new char [col][row]; 
-                    // check location of entrance to determine where to move first
-                    if(House[col][row] == enter){
-                        spot[col][row] = House[col][row];
-                        if(row == 0){
-                            if(House[col][row + 1] == open){
-                                // move down
-                                spot[col][row] = House[col][row + 1];
-                            }
+    }
+
+    // JA: This method should return a value or be declared void
+    public static char[][] Maze(char[][] House) {
+        char enter = '*';
+        char mirror1 = '/';
+        char mirror2 = '\\';
+        char open = '.';
+        char exit = '&';
+        for (int col = 0; col < House.length; col++) {
+            for (int row = 0; row < House[col].length; row++) {
+                char[][] spot = new char[col][row];
+                // check location of entrance to determine where to move first
+                if (House[col][row] == enter) {
+                    spot[col][row] = House[col][row];
+                    if (row == 0) {
+                        if (House[col][row + 1] == open) {
+                            // move down
+                            spot[col][row] = House[col][row + 1];
                         }
-                        else if(House[col + 1][row] == open){
+                    } else if (House[col + 1][row] == open) {
+                        // move right
+                        spot[col][row] = House[col + 1][row];
+                    } else if (House[col - 1][row] == open) {
+                        // move left
+                        spot[col][row] = House[col - 1][row];
+                    } else {
+                        // move up
+                        spot[col][row] = House[col][row - 1];
+                    }
+                }
+                while (row != House.length - 1) {
+                    if (House[col][row] == mirror1 || House[col][row] == mirror2) {
+                        if (House[col][row + 1] == open) {
+                            // move down
+                            spot[col][row] = House[col][row + 1];
+                        } else if (House[col + 1][row] == open) {
                             // move right
                             spot[col][row] = House[col + 1][row];
-                            }
-                        else if(House[col - 1][row] == open){
+                        } else if (House[col - 1][row] == open) {
                             // move left
                             spot[col][row] = House[col - 1][row];
-                            }
-                        else{
+                        } else {
                             // move up
                             spot[col][row] = House[col][row - 1];
                         }
                     }
-                    while(row != House.length - 1){
-                        if(House[col][row] == mirror1 || House[col][row] == mirror2){
-                            if(House[col][row + 1] == open){
-                                // move down
-                                spot[col][row] = House[col][row + 1];
-                            }
-                            else if(House[col + 1][row] == open){
-                                // move right
-                                spot[col][row] = House[col + 1][row];
-                                }
-                            else if(House[col - 1][row] == open){
-                                // move left
-                                spot[col][row] = House[col - 1][row];
-                                }
-                            else{
-                                // move up
-                                spot[col][row] = House[col][row - 1];
-                            }
-                        }
-                    }
-                    if(row == House.length - 1){
-                        House[col][row] = exit;
-                    }
-                    System.out.println(House[col][row]);
                 }
+                if (row == House.length - 1) {
+                    House[col][row] = exit;
+                }
+                System.out.println(House[col][row]);
             }
-        }                  
+        }
     }
 }
